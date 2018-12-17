a29404bedb7c9fcf2fed427fb9c3b0475d3762b5
alanwake <wjy1063896722@outlook.com>
2018-12-17 19:53:11 +0800

    v2.1

--- inc/common.h
+++ inc/common.h
@@ -6,13 +6,18 @@
 #include "STC8.h"
 #include "intrins.h"
 
-
+#ifdef FOSC_24000000
+    #define FOSC 24000000UL
+#endif
+#ifdef FOSC_11059200 
+    #define FOSC 11059200UL
+#endif
 #define null 0
 #define bool bit
-#define FOSC 24000000UL   //晶振频率
-#define LED P20
-#define KEY P32
-#define ADCPIN 2
+// #define FOSC 24000000UL   //晶振频率
+// #define LED P20
+// #define KEY P32
+// #define ADCPIN 2
 
 extern void delayus(unsigned int us);
 extern void delay(unsigned ms);
--- src/common.c
+++ src/common.c
@@ -1,44 +1,81 @@
 #include "common.h"
 #include "intrins.h"
 
-void Delay1us()		//@24.000MHz
-{
-	unsigned char i;
+#ifdef STC_Y6
 
-	i = 6;
-	while (--i);
-}
+    #ifdef FOSC_24000000
+        void Delay1us()		//@24.000MHz
+        {
+            unsigned char i;
 
-void Delay1ms()		//@24.000MHz
-{
-	unsigned char i, j;
+            i = 6;
+            while (--i);
+        }
 
-	_nop_();
-	i = 32;
-	j = 40;
-	do
-	{
-		while (--j);
-	} while (--i);
-}
+        void Delay1ms()		//@24.000MHz
+        {
+            unsigned char i, j;
+
+            _nop_();
+            i = 32;
+            j = 40;
+            do
+            {
+                while (--j);
+            } while (--i);
+        }
+    #endif
+
+    void delayus(unsigned int us)
+    {
+
+        while(us--)
+        {
+            Delay1us();
+        }
 
 
-void delayus(unsigned int us)
-{
+    }
 
-	while(us--)
-	{
-		Delay1us();
-	}
+    void delay(unsigned int ms)
+    {
+        while(ms--)
+        {
+            Delay1ms();
+        }
 
+    }
+#endif
 
-}
+#ifdef STC_Y1
+    #ifdef FOSC_11059200 
+        void Delay1ms()		//@11.0592MHz
+        {
+            unsigned char i, j;
 
-void delay(unsigned int ms)
-{
-	while(ms--)
-	{
-		Delay1ms();
-	}
+            _nop_();
+            i = 2;
+            j = 190;
+            do
+            {
+                while (--j);
+            } while (--i);
+        }
 
-}
+    #endif
+    void delay(unsigned int ms)
+    {
+        while(ms--)
+        {
+            Delay1ms();
+        }
+
+    }
+
+    void delayus(unsigned int us)
+    {
+
+        while(us--);
+
+    }
+#endif
